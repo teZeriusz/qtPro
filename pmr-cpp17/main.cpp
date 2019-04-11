9bf4a5a7a44ffb2ee5ecf7d57aadf32329feb4e4
Lukasz Wychrystenko <ext-lukasz.wychrystenko@here.com>
2019-04-11 11:06:54 +0200

    base on: https://www.youtube.com/watch?v=DyGNOjJUUuMi memory_resource: https://gcc.gnu.org/onlinedocs/libstdc++/manual/status.html#status.iso.201z https://libcxx.llvm.org/cxx1z_status.html

---
+++ pmr-cpp17/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+#include <experimental/memory_resource>
+
+using namespace std;
+
+
+struct CountingMemoryResource: public std::experimental::pmr::memory_resource {
+    explicit CountingMemoryResource(std::experimental::pmr::memory_resource * parent = std::experimental::pmr::get_default_resource()):
+        d_parent(parent), d_currentSize(0), d_maxSize(0), d_count(0) {
+    }
+
+    void * do_allocate(const size_t _Bytes, const size_t _Allign) override {
+        void * res = d_parent->allocate(_Bytes, _Allign);
+        d_currentSize += _Bytes;
+        if(d_currentSize > d_maxSize)
+            d_maxSize = d_currentSize;
+
+        ++d_count;
+        return res;
+    }
+
+    void do_deallocate(void * const _Ptr, const size_t _Bytes, size_t _Align) noexcept override {
+        d_parent->deallocate(_Ptr, _Bytes, _Align);
+        d_currentSize -= _Bytes;
+    }
+
+    virtual bool do_is_equal(const memory_resource& _That) const noexcept override {
+        return (this == &_That);
+    }
+
+private:
+    std::experimental::pmr::memory_resource * d_parent;
+    size_t d_currentSize, d_maxSize, d_count;
+};
+
+
+
+size_t unique(std::string & s1) {
+    CountingMemoryResource cmr;
+    std::experimental::pmr::set<char> a(&cmr);
+    a.insert(s1.begin(), s1.end());
+
+    return a.size();
+}
+
+int main() {
+    string s1("very long string with dummy text in it that only take space and some memory");
+    size_t foundCount = unique(s1);
+    cout << foundCount << endl;
+
+    return 0;
+}
